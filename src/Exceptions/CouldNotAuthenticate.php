98e3fe19dee2802557a63ca43dbdf2fb1a1d23ea
Yechiel <mhb@paritcher.com>
2024-11-20 12:17:41 -0500

    phpstan

--- src/Exceptions/CouldNotAuthenticate.php
+++ src/Exceptions/CouldNotAuthenticate.php
@@ -4,11 +4,11 @@ namespace Coxlr\RingCentral\Exceptions;
 
 use Exception;
 
-class CouldNotAuthenticate extends Exception
+final class CouldNotAuthenticate extends Exception
 {
     public static function operatorLoginFailed(): static
     {
-        return new static('Failed to log in operator extension');
+        return new self('Failed to log in operator extension');
     }
 
     public static function adminLoginFailed(): static
